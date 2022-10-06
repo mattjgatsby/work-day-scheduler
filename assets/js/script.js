4f6e72ad694e486ed06be87f8efa6227c60430e0
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-10-06 19:43:53 -0400

    added get item to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
+
 $(document).ready(function () {
   var today = moment();
 
@@ -34,3 +31,13 @@ $(document).ready(function () {
   updateHour();
   var interval = setInterval(updateHour, 15000);
 });
+
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
