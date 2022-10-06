1cebfdcab8f23095bb4f47e23e79497792ade100
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-10-05 21:49:53 -0400

    added time block function to add classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,5 +9,15 @@ var today = moment();
 
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-console.log("today", today);
+$(".time-block").each(function(){
+    var currentTime = moment().hours();
+    var targetTime = parseInt($(this).attr("id"));
 
+    if (targetTime === currentTime) {
+        $(this).addclass("present");
+    } else if (targetTime < currentTime) {
+        $(this).addclass("past");
+    } else {
+        $(this).addclass("future");
+    }
+});
