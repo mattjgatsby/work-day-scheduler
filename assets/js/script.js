464387583ddbc2419fddce072c032e5dbf27fe39
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-10-06 10:03:04 -0400

    formatted html, js. Added on click function for save button and localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,36 @@
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
 // THEN the saved events persist
-var today = moment();
+$(document).ready(function () {
+  var today = moment();
 
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+  $(".saveBtn").on("click", function () {
+    var value = $(this).siblings(".description").val();
 
-$(".time-block").each(function(){
+    var idHours = $(this).parent().attr("id");
+
+    localStorage.setItem(idHours, value);
+  });
+
+  $("#currentDay").text(today.format("MMM Do, YYYY"));
+  function updateHour() {
     var currentTime = moment().hours();
-    var targetTime = parseInt($(this).attr("id"));
+    $(".time-block").each(function () {
+      var targetTime = parseInt($(this).attr("id").split("-")[1]);
 
-    if (targetTime === currentTime) {
-        $(this).addclass("present");
-    } else if (targetTime < currentTime) {
-        $(this).addclass("past");
-    } else {
-        $(this).addclass("future");
-    }
+      if (targetTime < currentTime) {
+        $(this).addClass("past");
+      } else if (targetTime === currentTime) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+  updateHour();
+  var interval = setInterval(updateHour, 15000);
 });
